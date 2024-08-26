26a1f0fea84962fb39e7eec5c19c8af05b60e2b1
Zyfa-dev <muhammadzyfa5465@gmail.com>
2024-08-27 03:28:22 +0500

    RESPONSIVE ADDED

--- home.js
+++ home.js
@@ -1,4 +1,30 @@
 
+var navbar = document.getElementById('nav-js')
+var nav_click=document.querySelector('.hov_nav')
+var body=document.querySelector('body')
+var nav_show=document.querySelector('.full_layer')
+var exit=document.getElementById('exit')
+
+function showbox(){
+
+nav_click.addEventListener('click',(e)=>{
+    nav_show.style.display=`block`
+    e.stopPropagation()
+    // nav_show.style.background=`rgba(0,0,0,0.4)`
+    })
+}
+window.addEventListener('click',(e)=>{
+    if (e.target !== nav_click && e.target !== nav_show 
+        && !nav_show.contains(e.target)) {
+       nav_show.style.display=`none`
+       }
+    })
+
+
+
+
+
+
 var prev=document.getElementById('back_prev')
 var next=document.getElementById('next')
 var cards=document.getElementById('cards')
@@ -76,6 +102,49 @@ Prev.addEventListener('click',() =>{
 })
 
 
+//reponsive function
+
+var Prev=document.querySelector('.back')
+var nxt=document.querySelector('.nxt')
+var move_2=document.querySelector('.destinations-2')
+var loc_no_2=document.querySelectorAll('#loc_img-2').length
+var localvalue_2=0
+
+function loc_img_2(){
+    console.log('in function');
+    if (localvalue_2==1) {
+    move_2.style.transform=`translateX(-${localvalue_2*101}%)`
+    } else {
+    move_2.style.transform=`translateX(-${localvalue_2*101.6}%)`
+    }
+}
+
+nxt.addEventListener('click',() =>{
+    
+    if (localvalue_2==loc_no_2-1) {
+        localvalue_2=0
+
+    } 
+    else {
+        localvalue_2++
+    }
+    loc_img_2()
+})
+
+Prev.addEventListener('click',() =>{
+    console.log('button is clicked');
+    
+    if(localvalue_2==0) {
+        localvalue_2=loc_no_2-1
+    } else {
+        localvalue_2--
+    
+    }
+    loc_img_2()
+})
+
+
+
 
 var resort=document.querySelector('.all_resorts')
 var imges_res=document.querySelectorAll('#res_img').length
@@ -110,10 +179,125 @@ btn_prev.addEventListener('click',()=>{
         
     }
     else{
+        value_res--
+    }
+    resort_result();
+})
+
+
+//responsive funtion
+var resort2=document.querySelector('.all_resorts-2')
+var imges_res2=document.querySelectorAll('#res_img_2').length
+var btn_prev=document.querySelector('.btn_prev')
+var btn_next=document.querySelector('.btn_next')
+var value_res2=0
+
+function resort_result2(){
+    if (value_res2==1) {
+        resort2.style.transform=`translateX(-${value_res2*101}%)`
+        } else {
+            resort2.style.transform=`translateX(-${value_res2*102}%)`
+        }
+
+}
+
+btn_next.addEventListener('click',()=>{
+    if(value_res2==imges_res2-1){
+        value_res2=0
+    }
+    else{
+// console.log('else is opened');
+        value_res2++
+    }
+    resort_result2();
+})
+
+btn_prev.addEventListener('click',()=>{
+    if(value_res==0){
+        value_res=imges_res-4
+        console.log('working');
+        
+    }
+    else{
 // console.log('else is opened in prev');
         value_res--
     }
     resort_result();
+})
+
+btn_prev.addEventListener('click',()=>{
+    if(value_res2==0){
+        value_res2=imges_res2-1
+        console.log('working');
+        
+    }
+    else{
+        value_res2--
+    }
+    resort_result2();
+})
+
+
+
+var resort_2=document.querySelector('.all_resorts-second')
+var imges_res_2=document.querySelectorAll('#res_img-2').length
+var btn_next_2=document.querySelector('.btn_next-2')
+var btn_prev_2=document.querySelector('.btn_prev-2')
+var secondtime_value=0
+
+function result_resort_2(){
+    resort_2.style.transform=`translateX(-${secondtime_value*110}%)`
+}
+
+btn_next_2.addEventListener('click',()=>{
+    if (secondtime_value==imges_res_2-4){
+        secondtime_value=0
+        
+    } else {
+        secondtime_value++
+    }
+    result_resort_2()
+})
+
+btn_prev_2.addEventListener('click',()=>{
+    if (secondtime_value==0) {
+        secondtime_value=imges_res_2-4
+    } else {
+        secondtime_value--
+    }
+    result_resort_2()
+})
+
+
+
+//responsive function
+var resort_3=document.querySelector('.all_resorts-sec')
+var imges_res_3=document.querySelectorAll('#res_img-3').length
+var btn_next_2=document.querySelector('.btn_next-2')
+var btn_prev_2=document.querySelector('.btn_prev-2')
+var secondtime_value2=0
+
+function result_resort_3(){
+    resort_3.style.transform=`translateX(-${secondtime_value2*104}%)`
+}
+
+btn_next_2.addEventListener('click',()=>{
+    if (secondtime_value2==imges_res_3-1){
+        secondtime_value2=0
+        
+    } else {
+        secondtime_value2++
+    }
+    result_resort_3()
+})
+
+btn_prev_2.addEventListener('click',()=>{
+    if (secondtime_value2==0) {
+        secondtime_value2=imges_res_3-1
+    } else {
+        secondtime_value2--
+    }
+    result_resort_3()
 })
 
 
